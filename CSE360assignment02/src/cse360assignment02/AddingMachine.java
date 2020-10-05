aa2c321d249bf745f3955eea496ac29caf1c400e
Alec Warong <Alec Warong@Alec>
2020-10-05 01:02:23 -0700

    Created private String toString for the toString method

--- CSE360assignment02/src/cse360assignment02/AddingMachine.java
+++ CSE360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,9 +2,11 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String toString;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    toString = "0";
   }
   
   public int getTotal () {
@@ -13,14 +15,16 @@ public class AddingMachine {
   
   public void add (int value) {
 	  total = total + value;
+	  toString += "+" + value;
   }
 
   public void subtract (int value) {
 	  total = total - value;
+	  toString += "-" + value;
   }
 
   public String toString () {
-    return "";
+    return toString;
   }
 
   public void clear() {
