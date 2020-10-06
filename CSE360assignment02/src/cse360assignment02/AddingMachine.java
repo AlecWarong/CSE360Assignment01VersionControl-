c12744e360faf2ce117205323a159a21b9a6936f
Alec Warong <Alec Warong@Alec>
2020-10-05 20:37:51 -0700

    Finsihed clear method

--- CSE360assignment02/src/cse360assignment02/AddingMachine.java
+++ CSE360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,32 +1,35 @@
 package cse360assignment02;
 
 public class AddingMachine {
-  private int total;
-  private String toString;
-  
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-    toString = "0";
-  }
-  
-  public int getTotal () {
-    return total;
-  }
-  
-  public void add (int value) {
-	  total = total + value;
-	  toString += "+" + value;
-  }
 
-  public void subtract (int value) {
-	  total = total - value;
-	  toString += "-" + value;
-  }
+    private int total;
+    private String toString = "0";
 
-  public String toString () {
-    return toString;
-  }
+    public AddingMachine() {
 
-  public void clear() {
-  }
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void add(int value) {
+        toString += " + " + value ;
+        total = total + value;
+    }
+
+    public void subtract(int value) {
+        toString += " - " + value;
+        total = total - value;
+    }
+
+    public String toString() {
+        return toString;
+    }
+
+    public void clear() {
+    	total = 0;
+    }
 }
+
